b7a1838d8f526625f458e76096228b4b8ef4af0f
Mark Neptune <102503462+jubatwain@users.noreply.github.com>
2023-06-22 10:40:42 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+bill=0
+if size =="S":
+    bill = 15
+    if add_pepperoni =="Y":
+        bill +=2
+    else:
+        bill= 15
+    if extra_cheese =="Y":
+        bill +=1
+        print(f"Your final bill is: ${bill}")
+    else:
+        print(f"Your final bill is: ${bill}")
+elif size =="M":
+    bill = 20
+    if add_pepperoni =="Y":
+        bill +=3
+    else:
+        bill = 20
+    if extra_cheese =="Y":
+        bill +=1
+        print(f"Your final bill is: ${bill}")
+    else:
+        print(f"Your final bill is: ${bill}")
+elif size =="L":
+    bill = 25
+    if add_pepperoni =="Y":
+        bill +=3
+    else:
+        bill = 25
+    if extra_cheese =="Y":
+        bill +=1
+        print(f"Your final bill is: ${bill}")
+    else:
+        print(f"Your final bill is: ${bill}")
+else:
+    print(f"Your final bill is: ${bill}")
